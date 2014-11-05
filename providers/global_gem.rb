60c5f9febdf04b33733ce2121c41ca42a0420d14
Dean Webb <Dean.webb@careerbuilder.com>
2014-11-05 17:53:17 -0500

    version bump

--- providers/global_gem.rb
+++ providers/global_gem.rb
@@ -57,7 +57,7 @@ def gem_package_wrapper(exec_action, ruby_global_gemset)
     user        new_resource.user
     action      :nothing
   end
-  enc
+  end
   g.run_action(exec_action)
   new_resource.updated_by_last_action(true) if g.updated_by_last_action?
 end
